c166560b099365009fe8095f70600280ed4a26a7
behnam1369 <behnam.aghaali@yahoo.com>
2022-04-15 23:51:28 +0430

    Add unit test and integration test

---
+++ src/tests/Integration.test.js
@@ -0,0 +1,38 @@
+import {
+  render,
+  screen,
+} from '@testing-library/react';
+import { Provider } from 'react-redux';
+import '@testing-library/jest-dom';
+import configureStore from '../redux/configureStore';
+import App from '../App';
+import { getContinents } from '../redux/Continents';
+
+
+describe('Integration test', () => {
+  it('Asia should be in continents list', () => {
+    const param = [
+      {
+        name: 'Asia',
+        todayCases: 0,
+        todayDeaths: 0,
+      },
+      {
+        name: 'Africa',
+        todayCases: 0,
+        todayDeaths: 0,
+      },
+    ];
+    configureStore.dispatch(getContinents(param));
+    const Jsx = () => (
+      <Provider store={configureStore}>
+        <App />
+      </Provider>
+    );
+    const mockCountries = () => {
+      render(Jsx());
+    };
+    mockCountries();
+    expect(screen.getByText('Asia')).toBeInTheDocument();
+  });
+});
